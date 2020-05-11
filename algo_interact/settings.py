4fc782bba6c68cab7d600d72c0e54520e393eae3
Jay Ivan Gentallan <jaygentallan926@gmail.com>
2020-05-11 12:57:34 -0700

    init

--- algo_interact/settings.py
+++ algo_interact/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static/'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles");
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_ROOT = os.path.join(BASE_DIR, "build/static");
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
