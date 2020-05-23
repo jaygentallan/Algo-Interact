f1a47c4179ff5349eb307bda129b493c79f3aa74
jaygentallan <jaygentallan926@gmail.com>
2020-05-22 21:32:21 -0700

    Static Files

--- algo_interact/settings.py
+++ algo_interact/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from .secret import *
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -193,14 +193,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static/'),
-    os.path.join(BASE_DIR, 'media/profile_pics'),
+    os.path.join(BASE_DIR, 'staticfiles'),
+    #os.path.join(BASE_DIR, 'media/profile_pics'),
 ]
 
 
