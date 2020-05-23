358c46d2b2dfdeb4ce7278b21908591bc261c5eb
jaygentallan <jaygentallan926@gmail.com>
2020-05-23 03:56:19 -0700

    S3 Static Files

--- algo_interact/urls.py
+++ algo_interact/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     #re_path('.*', TemplateView.as_view(template_name='index.html')),
