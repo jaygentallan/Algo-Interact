64a0aba1ebb55237fd81012c7cd135156ddde968
jaygentallan <jaygentallan926@gmail.com>
2020-05-23 20:26:17 -0700

    S3 Bucket

---
+++ build/precache-manifest.fdb582e40a980098cb4c312780181222.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2c14b9d85ff03250823806a7ada439",
+    "url": "https://algointeract.s3.amazonaws.com/index.html"
+  },
+  {
+    "revision": "77583ee231e7aee3df14",
+    "url": "https://algointeract.s3.amazonaws.com/static/css/2.279ca4b4.chunk.css"
+  },
+  {
+    "revision": "845017633cc0148f8de4",
+    "url": "https://algointeract.s3.amazonaws.com/static/css/main.faa1af7c.chunk.css"
+  },
+  {
+    "revision": "77583ee231e7aee3df14",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/2.11ed9c2a.chunk.js"
+  },
+  {
+    "revision": "591f46f478d3e62205980cc9fb0c9b34",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/2.11ed9c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845017633cc0148f8de4",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/main.05e6b689.chunk.js"
+  },
+  {
+    "revision": "0f8bb5d9cfc4605f54ed",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/runtime-main.e4daff65.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "https://algointeract.s3.amazonaws.com/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "https://algointeract.s3.amazonaws.com/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "https://algointeract.s3.amazonaws.com/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d72f24593a0946048accd5ec14d3398.js"
+  "https://algointeract.s3.amazonaws.com/precache-manifest.fdb582e40a980098cb4c312780181222.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://algointeract.s3.amazonaws.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
