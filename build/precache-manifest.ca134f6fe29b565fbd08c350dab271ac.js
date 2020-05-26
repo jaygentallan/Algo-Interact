a6c61d1e59953e963426e772a3975e29d60d2821
jaygentallan <jaygentallan926@gmail.com>
2020-05-26 02:21:45 -0700

    Added User Dropdown

---
+++ build/precache-manifest.ca134f6fe29b565fbd08c350dab271ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f783c33bd14e2b227f1b7948c3291a",
+    "url": "https://algointeract.s3.amazonaws.com/index.html"
+  },
+  {
+    "revision": "ad66acd095f7f256ef6e",
+    "url": "https://algointeract.s3.amazonaws.com/static/css/2.aae2bc28.chunk.css"
+  },
+  {
+    "revision": "2ca48e6ae91e4ce88867",
+    "url": "https://algointeract.s3.amazonaws.com/static/css/main.d4a5c9ba.chunk.css"
+  },
+  {
+    "revision": "ad66acd095f7f256ef6e",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/2.7c530d98.chunk.js"
+  },
+  {
+    "revision": "3b1eff739416ff870321de018309ced7",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/2.7c530d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca48e6ae91e4ce88867",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/main.851cf03c.chunk.js"
+  },
+  {
+    "revision": "0f8bb5d9cfc4605f54ed",
+    "url": "https://algointeract.s3.amazonaws.com/static/js/runtime-main.e4daff65.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "https://algointeract.s3.amazonaws.com/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "https://algointeract.s3.amazonaws.com/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "https://algointeract.s3.amazonaws.com/static/media/revicons.2feb69cc.eot"
+  }
+]);
