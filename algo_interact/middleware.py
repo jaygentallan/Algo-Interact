611163495feac57628ddbdfa209665afe1c73148
jaygentallan <jaygentallan926@gmail.com>
2020-06-21 03:16:38 -0700

    Profile Page + REDUX Overhaul

--- algo_interact/middleware.py
+++ algo_interact/middleware.py
@@ -1,3 +1,5 @@
+import os
+
 def dev_cors_middleware(get_response):
         """
         Adds CORS headers for local testing only to allow the frontend, which is served on
@@ -6,7 +8,7 @@ def dev_cors_middleware(get_response):
         def middleware(request):
             response = get_response(request)
 
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+            response['Access-Control-Allow-Origin'] = os.environ.get('ALGOINTERACT_URL')
             response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, OPTIONS, DELETE, HEAD'
             response['Access-Control-Allow-Headers'] = 'Content-Type, X-CSRFToken'
             response['Access-Control-Allow-Credentials'] = 'true'
